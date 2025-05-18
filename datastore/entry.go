1c489a13bb4b66a125211f853ad65c490b68bc67
exluse <114081542+gloviee@users.noreply.github.com>
2025-05-18 13:38:02 +0300

    Update entry.go

--- datastore/entry.go
+++ datastore/entry.go
@@ -1,58 +1,67 @@
 package datastore
 
 import (
-	"bufio"
 	"encoding/binary"
-	"errors"
-	"fmt"
 	"io"
+	"iter"
+	"os"
 )
 
-type entry struct {
-	key, value string
+type kvPair struct {
+	key   string
+	value string
 }
 
-// 0           4    8     kl+8  kl+12     <-- offset
-// (full size) (kl) (key) (vl)  (value)
-// 4           4    ....  4     .....     <-- length
+func Serialize(pair kvPair) []byte {
+	kLen := len(pair.key)
+	vLen := len(pair.value)
+	buf := make([]byte, kLen+vLen+8)
 
-func (e *entry) Encode() []byte {
-	kl, vl := len(e.key), len(e.value)
-	size := kl + vl + 12
-	res := make([]byte, size)
-	binary.LittleEndian.PutUint32(res, uint32(size))
-	binary.LittleEndian.PutUint32(res[4:], uint32(kl))
-	copy(res[8:], e.key)
-	binary.LittleEndian.PutUint32(res[kl+8:], uint32(vl))
-	copy(res[kl+12:], e.value)
-	return res
+	binary.LittleEndian.PutUint32(buf[0:4], uint32(kLen))
+	copy(buf[4:], pair.key)
+	binary.LittleEndian.PutUint32(buf[4+kLen:8+kLen], uint32(vLen))
+	copy(buf[8+kLen:], pair.value)
+
+	return buf
 }
 
-func (e *entry) Decode(input []byte) {
-	e.key = decodeString(input[4:])
-	e.value = decodeString(input[len(e.key)+8:])
+func readString(r io.ReaderAt, offset int64) (string, int, error) {
+	header := make([]byte, 4)
+	if _, err := r.ReadAt(header, offset); err != nil {
+		return "", 0, err
+	}
+	length := int(binary.LittleEndian.Uint32(header))
+	content := make([]byte, length)
+	if _, err := r.ReadAt(content, offset+4); err != nil {
+		return "", 0, err
+	}
+	return string(content), 4 + length, nil
 }
 
-func decodeString(v []byte) string {
-	l := binary.LittleEndian.Uint32(v)
-	buf := make([]byte, l)
-	copy(buf, v[4:4+int(l)])
-	return string(buf)
-}
-
-func (e *entry) DecodeFromReader(in *bufio.Reader) (int, error) {
-	sizeBuf, err := in.Peek(4)
+func LoadEntry(r io.ReaderAt, offset int64) (kvPair, error) {
+	k, kSize, err := readString(r, offset)
 	if err != nil {
-		if errors.Is(err, io.EOF) {
-			return 0, err
+		return kvPair{}, err
+	}
+	v, _, err := readString(r, offset+int64(kSize))
+	if err != nil {
+		return kvPair{}, err
+	}
+	return kvPair{k, v}, nil
+}
+
+func Stream(f *os.File) iter.Seq[kvPair] {
+	return func(yield func(kvPair) bool) {
+		var offset int64
+		for {
+			pair, err := LoadEntry(f, offset)
+			if err != nil {
+				return
+			}
+			offset += int64(len(pair.key)+len(pair.value) + 8)
+			if !yield(pair) {
+				return
+			}
 		}
-		return 0, fmt.Errorf("DecodeFromReader, cannot read size: %w", err)
 	}
-	buf := make([]byte, int(binary.LittleEndian.Uint32(sizeBuf)))
-	n, err := in.Read(buf[:])
-	if err != nil {
-		return n, fmt.Errorf("DecodeFromReader, cannot read record: %w", err)
-	}
-	e.Decode(buf)
-	return n, nil
 }
